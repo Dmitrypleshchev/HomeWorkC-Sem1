b34103559e9f672a5acbfc11117cfb8fe3a9d876
Dmitry Pleshchev <dmitrydevlax@yandex.ru>
2023-03-29 01:04:32 +0500

    Task 4 added

---
+++ Task 4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход чтри числа и выдает максимальное из этих чисел.
+int a = 0;
+int b = 0;
+int c = 0;
+int max = a;
+Console.WriteLine("Введите число a:");
+int.TryParse(Console.ReadLine(), out a);
+Console.WriteLine("Введите число b:");
+int.TryParse(Console.ReadLine(), out b);
+Console.WriteLine("Введите число c:");
+int.TryParse(Console.ReadLine(), out c);
+
+if ((a > b) && (a > c)) max = a;
+if ((b > a) && (b > c)) max = b;
+if ((c > b) && (c > a)) max = c;
+
+Console.WriteLine(max);
